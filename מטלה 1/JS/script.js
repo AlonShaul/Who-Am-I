b450d2c3751c605cb9c204087ee827bfa2d3de1e
AlonShaul <93150930+AlonShaul@users.noreply.github.com>
2022-11-28 00:05:25 +0200

    Add files via upload

---
+++ מטלה 1/JS/script.js
@@ -0,0 +1,13 @@
+document.getElementById("b").style.visibility = "hidden";
+document.getElementById("a").style.visibility = "visible";
+
+document.getElementById("btn1").onclick = function() {  
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("a").style.visibility = "hidden";
+    document.getElementById("btn1").style.visibility = "hidden";
+};
+
+
+type="module" 
+src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
